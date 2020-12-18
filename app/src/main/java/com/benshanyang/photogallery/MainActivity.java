b46210bd68455079c04cd043d3fb4aaf64fd25da
杨宽 <1147694857@qq.com>
2020-12-18 10:59:23 +0800

    编写图片浏览器功能

--- app/src/main/java/com/benshanyang/photogallery/MainActivity.java
+++ app/src/main/java/com/benshanyang/photogallery/MainActivity.java
@@ -16,22 +16,24 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private PhotoGallery gallery;
+    private List<String> list = Arrays.asList("https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=518565351,741314501&fm=26&gp=0.jpg", "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=820556762,652942924&fm=26&gp=0.jpg", "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=512426052,3777200390&fm=26&gp=0.jpg");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         gallery = new PhotoGallery<>(new PhotoGalleryable<String>() {
             @Override
             public List<String> getUrls() {
-                return Arrays.asList("https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=518565351,741314501&fm=26&gp=0.jpg", "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=820556762,652942924&fm=26&gp=0.jpg", "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=512426052,3777200390&fm=26&gp=0.jpg");
+                return list;
             }
 
             @Override
             public void bindData(String data, ImageView imageView) {
                 Glide.with(MainActivity.this).load(data).into(imageView);
             }
-        });
+        }).showIndicator(list.size() == 1 ? PhotoGallery.NONE : PhotoGallery.CIRCLE).setCurrentItem(1);
     }
 
     public void showDialog(View view) {
--- photo_gallery/src/main/java/com/benshanyang/photo_gallery/PhotoGallery.java
+++ photo_gallery/src/main/java/com/benshanyang/photo_gallery/PhotoGallery.java
@@ -15,6 +15,7 @@ import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.annotation.IntDef;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
@@ -22,6 +23,8 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.benshanyang.photo_gallery.photoview.PhotoView;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,18 +34,25 @@ import java.util.List;
  * @Author: YangKuan
  * @Date: 2020/12/16 15:24
  */
-public class PhotoGallery<T> extends DialogFragment implements View.OnClickListener {
+public class PhotoGallery<T> extends DialogFragment {
 
-    private TextView tvNumberIndicator;
-    private RelativeLayout indicatorLayout;
-    private RadioGroup indicatorGroup;
+    public static final int NONE = 0;//不显示指示器
+    public static final int TEXT = 1;//设置指示器是文字类型的
+    public static final int CIRCLE = 2;//设置指示器是圆点类型的
 
-    private int count = 0;
+    private ViewPager viewPager;//图片浏览控件
+    private TextView tvNumberIndicator;//文字指示器
+    private RelativeLayout indicatorLayout;//底部圆点指示器父布局
+    private RadioGroup indicatorGroup;//底部圆点指示器
+
+    private int count = 0;//图片的数量
+    private int indicatorType = 1;//指示器默认文字类型的
+    private int currentItemIndex = 0;//当前ViewPager初始显示的页的索引值
     private List<PhotoView> photoViewList = null;
-    private PhotoGalleryable<T> photoGalleryable;
+    private PhotoGalleryable<T> photoGalleryAble;
 
-    public PhotoGallery(PhotoGalleryable<T> photoGalleryable) {
-        this.photoGalleryable = photoGalleryable;
+    public PhotoGallery(PhotoGalleryable<T> photoGalleryAble) {
+        this.photoGalleryAble = photoGalleryAble;
     }
 
     @Override
@@ -68,22 +78,31 @@ public class PhotoGallery<T> extends DialogFragment implements View.OnClickListe
         tvNumberIndicator = (TextView) view.findViewById(R.id.tv_number_indicator);
         indicatorGroup = (RadioGroup) view.findViewById(R.id.indicator_group);
         indicatorLayout = (RelativeLayout) view.findViewById(R.id.indicator_layout);
-        view.findViewById(R.id.iv_close).setOnClickListener(this);
-        ViewPager viewPager = (ViewPager) view.findViewById(R.id.view_pager);
+        viewPager = (ViewPager) view.findViewById(R.id.view_pager);
+        view.findViewById(R.id.iv_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        resetIndicator();//设置指示器类型
+
         viewPager.setAdapter(new GalleryAdapter() {
 
             @Override
             public void init() {
-                if (photoGalleryable != null) {
+                if (photoGalleryAble != null) {
                     photoViewList = new ArrayList<>();
-                    List<T> urls = photoGalleryable.getUrls();
+                    List<T> urls = photoGalleryAble.getUrls();
                     count = (urls != null ? urls.size() : 0);
                     tvNumberIndicator.setText(String.format("%s/%s", "1", count));//顶部文字指示器
                     for (int i = 0; i < count; i++) {
                         PhotoView photoView = new PhotoView(getContext());
-                        photoGalleryable.bindData(urls.get(i), photoView);
+                        photoGalleryAble.bindData(urls.get(i), photoView);
                         photoViewList.add(photoView);
 
+                        //底部圆点指示器
                         RadioGroup.LayoutParams params = new RadioGroup.LayoutParams(dp2px(8f), dp2px(8f));
                         params.leftMargin = dp2px(3f);
                         params.rightMargin = dp2px(3f);
@@ -145,8 +164,18 @@ public class PhotoGallery<T> extends DialogFragment implements View.OnClickListe
 
             @Override
             public void onPageSelected(int position) {
-                tvNumberIndicator.setText(String.format("%s/%s", position + 1, count));//顶部文字指示器
-                ((RadioButton) indicatorGroup.getChildAt(position)).setChecked(true);
+                if (tvNumberIndicator != null && tvNumberIndicator.getVisibility() == View.VISIBLE) {
+                    //顶部文字指示器
+                    tvNumberIndicator.setText(String.format("%s/%s", position + 1, count));
+                }
+                if (indicatorLayout != null && indicatorGroup != null && indicatorLayout.getVisibility() == View.VISIBLE) {
+                    //底部圆点指示器
+                    try {
+                        ((RadioButton) indicatorGroup.getChildAt(position)).setChecked(true);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
             }
 
             @Override
@@ -154,7 +183,7 @@ public class PhotoGallery<T> extends DialogFragment implements View.OnClickListe
 
             }
         });
-        viewPager.setCurrentItem(0);
+        viewPager.setCurrentItem(currentItemIndex);
     }
 
     @Override
@@ -169,42 +198,96 @@ public class PhotoGallery<T> extends DialogFragment implements View.OnClickListe
         }
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.iv_close) {
-            dismiss();
-        }
-    }
-
-    public PhotoGallery showTextIndicator() {
-        if (tvNumberIndicator != null) {
-            tvNumberIndicator.setVisibility(View.VISIBLE);
-        }
-        if (indicatorLayout != null) {
-            indicatorLayout.setVisibility(View.GONE);
-        }
-        return PhotoGallery.this;
-    }
-
-    public PhotoGallery showCircleIndicator() {
-        if (indicatorLayout != null) {
-            indicatorLayout.setVisibility(View.VISIBLE);
-        }
-        if (tvNumberIndicator != null) {
-            tvNumberIndicator.setVisibility(View.GONE);
+    /**
+     * 设置初始显示第几张图片
+     *
+     * @param index 对应图片的索引值
+     * @return 返回当前类的引用
+     */
+    public PhotoGallery<T> setCurrentItem(int index) {
+        currentItemIndex = index;
+        if (viewPager != null) {
+            viewPager.post(new Runnable() {
+                @Override
+                public void run() {
+                    viewPager.setCurrentItem(currentItemIndex);
+                }
+            });
         }
         return PhotoGallery.this;
     }
 
     /**
+     * 设置图片浏览器的指示器类型
+     *
+     * @param type 指示器类型 共有3种：PhotoGallery.NONE-不显示指示器、PhotoGallery.TEXT-指示器是文字类型的、PhotoGallery.CIRCLE-指示器是圆点类型的
+     * @return 返回当前类的引用
+     */
+    public PhotoGallery<T> showIndicator(@Indicator int type) {
+        indicatorType = type;
+        resetIndicator();
+        return PhotoGallery.this;
+    }
+
+    private void resetIndicator() {
+        switch (indicatorType) {
+            case NONE:
+                //无指示器
+                if (indicatorLayout != null && indicatorLayout.getVisibility() == View.VISIBLE) {
+                    //圆点指示器
+                    indicatorLayout.setVisibility(View.GONE);
+                }
+                if (tvNumberIndicator != null && tvNumberIndicator.getVisibility() == View.VISIBLE) {
+                    //文字指示器
+                    tvNumberIndicator.setVisibility(View.GONE);
+                }
+                break;
+            case CIRCLE:
+                //底部圆点类型的指示器
+                if (indicatorLayout != null && indicatorLayout.getVisibility() == View.GONE) {
+                    //圆点指示器
+                    indicatorLayout.setVisibility(View.VISIBLE);
+                }
+                if (tvNumberIndicator != null && tvNumberIndicator.getVisibility() == View.VISIBLE) {
+                    //文字指示器
+                    tvNumberIndicator.setVisibility(View.GONE);
+                }
+                break;
+            case TEXT:
+            default:
+                //文字类型的指示器
+                if (indicatorLayout != null && indicatorLayout.getVisibility() == View.VISIBLE) {
+                    //圆点指示器
+                    indicatorLayout.setVisibility(View.GONE);
+                }
+                if (tvNumberIndicator != null && tvNumberIndicator.getVisibility() == View.GONE) {
+                    //文字指示器
+                    tvNumberIndicator.setVisibility(View.VISIBLE);
+                }
+                break;
+        }
+    }
+
+    /**
      * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
      *
-     * @param dpValue
-     * @return
+     * @param dpValue dp值
+     * @return 返回px值
      */
     private int dp2px(float dpValue) {
-        final float scale = getContext().getResources().getDisplayMetrics().density;
-        return (int) (dpValue * scale + 0.5f);
+        int pxValue = 0;
+        Context context = getContext();
+        if (context != null) {
+            final float scale = context.getResources().getDisplayMetrics().density;
+            pxValue = (int) (dpValue * scale + 0.5f);
+        }
+        return pxValue;
+    }
+
+    // 注解仅存在于源码中，在class字节码文件中不包含
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({NONE, TEXT, CIRCLE})
+    private @interface Indicator {
     }
 
 }
