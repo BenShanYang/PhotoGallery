f5d91d694785ed05806ee374f9f8b764110559fb
杨宽 <1147694857@qq.com>
2020-12-18 14:43:00 +0800

    为图片添加点击事件和长点击事件

--- app/src/main/java/com/benshanyang/photogallery/MainActivity.java
+++ app/src/main/java/com/benshanyang/photogallery/MainActivity.java
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.benshanyang.photo_gallery.OnItemClickListener;
 import com.benshanyang.photo_gallery.OnItemLongClickListener;
 import com.benshanyang.photo_gallery.PhotoGallery;
 import com.benshanyang.photo_gallery.PhotoGalleryable;
@@ -34,17 +36,28 @@ public class MainActivity extends AppCompatActivity {
             public void bindData(String data, ImageView imageView) {
                 Glide.with(MainActivity.this).load(data).into(imageView);
             }
-        }).showIndicator(list.size() == 1 ? PhotoGallery.NONE : PhotoGallery.TEXT).setCurrentItem(1);
-
+        });
+        //设置指示器的类型
+        // 共有3种：PhotoGallery.NONE-不显示指示器、PhotoGallery.TEXT-指示器是文字类型的、PhotoGallery.CIRCLE-指示器是圆点类型的
+        gallery.showIndicator(list.size() == 1 ? PhotoGallery.NONE : PhotoGallery.TEXT);
+        gallery.setCurrentItem(1);//设置初始显示第几张图片
         gallery.setOnItemLongClickListener(new OnItemLongClickListener<String>() {
             @Override
             public void onLongClick(View view, String bean, int position) {
+                //图片的长点击事件
                 new AlertDialog.Builder(MainActivity.this)
-                        .setMessage("您确认要保存该图片")
+                        .setMessage("您确认要保存该图片?")
                         .setPositiveButton("确定", null)
                         .setNeutralButton("取消", null)
                         .create()
                         .show();
+            }
+        });
+        gallery.setOnItemClickListener(new OnItemClickListener<String>() {
+            @Override
+            public void onClick(View view, String bean, int position) {
+                //图片的点击事件
+                Toast.makeText(MainActivity.this, String.format("您点击了第%s张图片", position + 1), Toast.LENGTH_SHORT).show();
             }
         });
     }
